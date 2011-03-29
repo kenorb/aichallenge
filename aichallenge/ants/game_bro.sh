44048e97ad764da0d40e530e2ee189d40a760993
kenorb <kenorb@gmail.com>
2011-03-29 11:06:52 +0000

    added run script for bro bot
    git-svn-id: https://mycognitive.unfuddle.com/svn/mycognitive_ants@43 9bf06172-cb9f-4928-8f50-0145d8ce07ba

---
+++ aichallenge/ants/game_bro.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh -x
+./playgame.py -v --seed 42 --output_dir viewer/bro_viewer --turns 20 --map_file maps/random4.txt "python dist/sample_bots/python/HunterBot.py" "python dist/sample_bots/python/keth.py" "python bots/bro/bro.py" "python dist/sample_bots/python/HunterBot.py"
+(netstat -na | grep .8000 > /dev/null) || sh ./python_server.sh > /dev/null &
+open http://localhost:8000/viewer/bro_viewer/viewer.html
