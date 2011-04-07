176cf73eae942b782afb8566eea9e38aa858b692
kenorb <kenorb@gmail.com>
2011-04-07 19:17:14 +0000

    changed arguments
    git-svn-id: https://mycognitive.unfuddle.com/svn/mycognitive_ants@54 9bf06172-cb9f-4928-8f50-0145d8ce07ba

--- aichallenge/ants/game_bro.sh
+++ aichallenge/ants/game_bro.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh -x
 (netstat -na | grep .8000 > /dev/null) || sh ./python_server.sh > /dev/null &
-./playgame.py -v --seed 42 --output_dir viewer/bro_viewer --turns 20 --map_file maps/random4.txt "python dist/sample_bots/python/HunterBot.py" "python dist/sample_bots/python/keth.py" "python bots/bro/bro.py" "python dist/sample_bots/python/HunterBot.py" \
+./playgame.py --turns 200 -v --seed 42 --output_dir viewer/bro_viewer --map_file maps/random4.txt "wine bots/KethBot/KethBot.exe" "java -jar bots/StoBotAdapt/sto003_AdaptToContestbotLk.jar" "python bots/bro/bro.py" "python dist/sample_bots/python/HunterBot.py" \
     && open http://localhost:8000/viewer/bro_viewer/viewer.html
