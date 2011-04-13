d535ce3154a897fa7cad4f3566849bd76b5edfd7
kenorb <kenorb@gmail.com>
2011-04-13 22:26:14 +0000

    Feed-forward neural network for python (ffnet) - http://ffnet.sourceforge.net/
    git-svn-id: https://mycognitive.unfuddle.com/svn/mycognitive_ants@78 9bf06172-cb9f-4928-8f50-0145d8ce07ba

---
+++ aichallenge/ants/bots/bro/ffnet/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from numpy.distutils.core import Extension
+from _version import version
+
+ext1 = Extension(name = 'ffnet.fortran._ffnet',
+                 sources = ['fortran/ffnet.f'])
+
+ext2 = Extension(name = 'ffnet.fortran._pikaia',
+                 sources = ['fortran/pikaia.f'])
+
+if __name__ == "__main__":
+    from numpy.distutils.core import setup
+    setup(name              = 'ffnet',
+          version           = version,
+          description       = 'Feed-forward neural network solution for python',
+          long_description  = '"ffnet" is a fast and easy-to-use feed-forward neural network training solution for python. Many nice features are implemented: arbitrary network connectivity, automatic data normalization, very efficient training tools, network export to fortran code...',
+          author            = 'Marek Wojciechowski',
+          author_email      = 'mwojc@p.lodz.pl',
+          url               = 'ffnet.sourceforge.net',
+          license           = 'GPL',
+          platforms         = 'Posix, Windows',
+          package_dir       = {'ffnet': '.'},
+          py_modules        = ['ffnet.ffnet', 
+                               'ffnet.pikaia', 
+                               'ffnet._tests', 
+                               'ffnet._version',
+                               'ffnet.fortran.__init__',
+                               'ffnet.tools._py2f',
+                               'ffnet.tools._py2j',
+                               'ffnet.tools.drawffnet'],
+          ext_modules       = [ext1, ext2],
+          data_files        = [('ffnet/examples',       ['examples/xor.py',
+                                                         'examples/ocr.py',
+                                                         'examples/sin.py',
+                                                         'examples/stock.py']),
+                               ('ffnet/examples/data',  ['examples/data/ocr.dat',
+                                                         'examples/data/black-scholes.dat']),
+                               ('ffnet/fortran',        ['fortran/ffnet.f']),
+                               ('ffnet/java/org/ffnet', ['java/org/ffnet/FFNet.java'])]
+          )
