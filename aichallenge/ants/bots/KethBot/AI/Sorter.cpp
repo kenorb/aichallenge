a32ab497e4fd9e29c1188e7a222943a435742b06
Keth <keth@kwketh.com>
2011-09-16 17:53:33 +0000

    Inconsistency of std::sort fixed
    git-svn-id: https://mycognitive.unfuddle.com/svn/mycognitive_ants@94 9bf06172-cb9f-4928-8f50-0145d8ce07ba

--- aichallenge/ants/bots/KethBot/AI/Sorter.cpp
+++ aichallenge/ants/bots/KethBot/AI/Sorter.cpp
@@ -6,11 +6,22 @@
 
 bool SortFunction(int a, int b, int c, int d, int e, int f, bool symetric)
 {
+    double d1;
+    double d2;
     if (symetric) {
-        return distance_fast(a, b, e, f) < distance_fast(c, d, e, f);
+        d1 = distance_fast(a, b, e, f);
+        d2 = distance_fast(c, d, e, f);
     } else {
-        return distance_real(a, b, e, f) < distance_real(c, d, e, f);
+        d1 = distance_real(a, b, e, f);
+        d2 = distance_real(c, d, e, f);
     }
+    if (d1 == d2) {
+        if (a == c) {
+            return b < d;
+        }
+        return a < c;
+    }
+    return d1 < d2;
 }
 
 
