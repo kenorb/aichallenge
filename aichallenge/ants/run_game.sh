0fa39322f061fb888f44163f9d1c1f09047ca008
kenorb <kenorb@gmail.com>
2011-09-18 20:04:20 +0000

    no seed and outout_dir option, removed and replaced with correct one
    git-svn-id: https://mycognitive.unfuddle.com/svn/mycognitive_ants@103 9bf06172-cb9f-4928-8f50-0145d8ce07ba

--- aichallenge/ants/run_game.sh
+++ aichallenge/ants/run_game.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh -x
-./playgame.py -v --seed 42 --output_dir viewer --turns 100 --map_file maps/random4.txt "python dist/sample_bots/python/HunterBot.py" "python dist/sample_bots/python/keth.py" "python bots/bro/bro.py" "python dist/sample_bots/python/HunterBot.py" > /dev/null
+./playgame.py --engine_seed 42 --player_seed 42 --food none --end_wait=0.25 --verbose --log_dir game_logs --turns 30 --turns 100 --map_file maps/random4.txt "python dist/sample_bots/python/HunterBot.py" "python dist/sample_bots/python/keth.py" "python bots/bro/bro.py" "python dist/sample_bots/python/HunterBot.py"
 
