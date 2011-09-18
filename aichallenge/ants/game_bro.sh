6ff240ae2485e86b59331aa24f0a7f881a929908
kenorb <kenorb@gmail.com>
2011-09-18 21:53:40 +0000

    updated run options
    git-svn-id: https://mycognitive.unfuddle.com/svn/mycognitive_ants@108 9bf06172-cb9f-4928-8f50-0145d8ce07ba

--- aichallenge/ants/game_bro.sh
+++ aichallenge/ants/game_bro.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env sh -x
-(netstat -na | grep .8000 > /dev/null) || sh ./python_server.sh > /dev/null &
-./playgame.py --turns 20 -v --seed 42 --output_dir viewer/bro_viewer --map_file maps/random4.txt "wine bots/KethBot/KethBot.exe" "java -jar bots/StoBotAdapt/sto003_AdaptToContestbotLk.jar" "python bots/bro/bro.py" "python dist/sample_bots/python/HunterBot.py" \
-    && open http://localhost:8000/viewer/bro_viewer/viewer.html
+if [ -n $1 ]; then
+  TURNS=$1
+else
+  TURNS=100
+fi
+./playgame.py $2 $3 $4 $5 $6 -R -S -I -O -E -e --engine_seed 42 --player_seed 42 --food 1 --end_wait=0.25 -v --log_dir game_logs --turns $TURNS --map_file maps/random4.txt "wine bots/KethBot/KethBot1908.exe" "java -jar bots/StoBotAdapt/sto003_AdaptToContestbotLk.jar" "python bots/bro/bro.py" "wine bots/KethBot/KethBot1809.exe"
+# "python dist/sample_bots/python/HunterBot.py" "python dist/sample_bots/python/GreedyBot.py"
+
