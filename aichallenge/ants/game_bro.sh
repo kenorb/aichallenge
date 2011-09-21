7cd443240b3ed307fbd51e84a22a67875898a8dd
kenorb <kenorb@gmail.com>
2011-09-21 15:25:43 +0000

    git synch
    git-svn-id: https://mycognitive.unfuddle.com/svn/mycognitive_ants@116 9bf06172-cb9f-4928-8f50-0145d8ce07ba

--- aichallenge/ants/game_bro.sh
+++ aichallenge/ants/game_bro.sh
@@ -4,7 +4,7 @@ if [ -n "$1" ]; then
 else
   TURNS=100
 fi
-./playgame.py $2 $3 $4 $5 $6 -R -S -I -O -E --engine_seed 42 --player_seed 42 --food 1 --end_wait=0.25 -v --log_dir game_logs --turns $TURNS --map_file maps/random4.txt "wine bots/KethBot/KethBot1908.exe" "java -jar bots/StoBotAdapt/sto003_AdaptToContestbotLk.jar" "python bots/bro/bro.py" "wine bots/KethBot/KethBot1809.exe"
+./playgame.py $2 $3 $4 $5 $6 -R -S -I -O -E --engine_seed 42 --player_seed 42 --food 1 --end_wait=0.25 -v --log_dir game_logs --turns $TURNS --map_file maps/random4.txt "wine bots/KethBot/KethBot1908.exe" "java -jar bots/StoBotAdapt/sto003_AdaptToContestbotLk.jar" "python bots/bro/bro.py -det" "wine bots/KethBot/KethBot1809.exe"
 # -e
 # "python dist/sample_bots/python/HunterBot.py" "python dist/sample_bots/python/GreedyBot.py"
 # attackradius2 is 5. viewradius2 is 77. spawnradius2 is 1.
